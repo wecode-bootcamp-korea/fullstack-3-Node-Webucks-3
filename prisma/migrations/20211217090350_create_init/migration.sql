f0aa1cc5f4186a7ef8db436a5f86d0026706dc21
guminki <rnalsrl93@gmail.com>
2021-12-17 18:28:29 +0900

    mission 6 완료

---
+++ prisma/migrations/20211217090350_create_init/migration.sql
@@ -0,0 +1,68 @@
+-- CreateTable
+CREATE TABLE `categories` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `products` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `korean_name` VARCHAR(191) NOT NULL,
+    `english_name` VARCHAR(191) NOT NULL,
+    `category_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `images` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `image_url` VARCHAR(1000) NOT NULL,
+    `product_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `allergies` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `products_allergies` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `product_id` INTEGER NOT NULL,
+    `allergy_id` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `nutritions` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `product_id` INTEGER NOT NULL,
+    `amount` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `products` ADD CONSTRAINT `products_category_id_fkey` FOREIGN KEY (`category_id`) REFERENCES `categories`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `images` ADD CONSTRAINT `images_product_id_fkey` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `products_allergies` ADD CONSTRAINT `products_allergies_product_id_fkey` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `products_allergies` ADD CONSTRAINT `products_allergies_allergy_id_fkey` FOREIGN KEY (`allergy_id`) REFERENCES `allergies`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE `nutritions` ADD CONSTRAINT `nutritions_product_id_fkey` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
--- server.js
+++ server.js
@@ -1,1 +1,56 @@
-console.log(`We're bringing sexyback`)
+const http = require("http");
+const express = require("express");
+const { PrismaClient } = require("@prisma/client");
+const { categories } = require("./categories");
+const {
+	products,
+	createProduct,
+	updateProduct,
+	deleteProduct,
+} = require("./products");
+const { detail } = require("./detail");
+
+const app = express();
+app.use(express.json());
+
+const prisma = new PrismaClient();
+
+app.post("/users/signup", async (req, res) => {
+	try {
+		const { email, name, password } = req.body;
+
+		console.log("email :", email, "name :", name);
+
+		const createUser =
+			await prisma.$queryRaw`INSERT INTO users (email, name, password) VALUES (${email}, ${name}, ${password})`;
+
+		return res.status(201).json({ message: "CREATED" });
+	} catch (err) {
+		console.log(err);
+		return res.status(500).json({ message: err.message });
+	}
+});
+
+app.get("/", (req, res) => {
+	res.json({ message: "/ backend start" });
+});
+
+app.get("/products/categories", categories);
+app.get("/products", products);
+app.get("/products/2", detail);
+app.post("/products", createProduct);
+app.put("/products", updateProduct);
+app.delete("/products", deleteProduct);
+
+const server = http.createServer(app);
+
+const start = async () => {
+	try {
+		server.listen(8000, () => console.log("Sever is listening on 8000"));
+	} catch (err) {
+		console.log(err);
+		await prisma.$disconnect();
+	}
+};
+
+start();
